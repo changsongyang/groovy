b9b00b9129ce4e06c76f7080c81f6771aa47f880
github-action-benchmark <github@users.noreply.github.com>
2026-09-22 13:13:38 +0000

    add Compiler Performance (customSmallerIsBetter) benchmark result for 7add18c8ed791e698b425c7431f54d5bdea7a751

--- dev/bench/perf/compiler/data.js
+++ dev/bench/perf/compiler/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1789997537070,
+  "lastUpdate": 1790082817924,
   "repoUrl": "https://github.com/changsongyang/groovy",
   "entries": {
     "Compiler Performance": [
@@ -5448,6 +5448,56 @@ window.BENCHMARK_DATA = {
             "name": "compile@groovy-5",
             "value": 460.30666666666673,
             "range": "±20.19",
+            "unit": "ms",
+            "extra": "5.1.2"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Paul King",
+            "username": "paulk-asert",
+            "email": "paulk@asert.com.au"
+          },
+          "committer": {
+            "name": "Paul King",
+            "username": "paulk-asert",
+            "email": "paulk@asert.com.au"
+          },
+          "id": "7add18c8ed791e698b425c7431f54d5bdea7a751",
+          "message": "add 5.1.X to SECURITY.md",
+          "timestamp": "2026-09-09T02:08:21Z",
+          "url": "https://github.com/changsongyang/groovy/commit/7add18c8ed791e698b425c7431f54d5bdea7a751"
+        },
+        "date": 1790082812120,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "compile@current",
+            "value": 584.44,
+            "range": "±26.14",
+            "unit": "ms",
+            "extra": "current"
+          },
+          {
+            "name": "compile@groovy-3",
+            "value": 682.7733333333333,
+            "range": "±226.56",
+            "unit": "ms",
+            "extra": "3.0.25"
+          },
+          {
+            "name": "compile@groovy-4",
+            "value": 578.8233333333334,
+            "range": "±139.91",
+            "unit": "ms",
+            "extra": "4.0.33"
+          },
+          {
+            "name": "compile@groovy-5",
+            "value": 487.04,
+            "range": "±23.75",
             "unit": "ms",
             "extra": "5.1.2"
           }
